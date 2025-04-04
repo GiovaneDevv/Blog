3660e8787e672c265acb02035ae5f6655d639a89
GiovaneDev <ja1430279@gmail.com>
2025-04-04 11:07:40 -0300

    Styling the menu.

--- src/Componets/Header/index.js
+++ src/Componets/Header/index.js
@@ -1,6 +1,27 @@
+import logosvg  from '../../svg/logo.svg.png';
+
 const Header = () =>{
+
+    const descLogo = "Essa é a logo do Blog."
+
    return (
-    <h1>Header.js</h1>
+     <>
+       <header className='flex-space-between'>
+          <div  className="logo">
+               <img src= {logosvg} alt={descLogo} />
+          </div>
+
+          <div  className="search">
+               <input type="text" className="input-search" name="search" />
+          </div>
+
+          <ul  className="menu">
+              <li><a href="#" className="nav-link"> Categories</a></li>
+              <li><a href="#" className="nav-link"> About</a></li>
+              <li><a href="#" className="nav-link"> Contact</a></li>
+          </ul>
+       </header>
+     </>
    )
 }
 export default Header;
